92bb110ce73e73e59e5edd353a0d4854c8c39bf8
REYES <153043081+MESIASREYES@users.noreply.github.com>
2024-04-06 13:05:09 -0600

    Update etiquetar-owner.js

--- plugins/etiquetar-owner.js
+++ plugins/etiquetar-owner.js
@@ -3,7 +3,7 @@ let handler = async (m, {conn, text}) => {
 
 conn.reply(m.chat, `*🌩 ️𝗡𝗼 𝗘𝘁𝗶𝗾𝘂𝗲𝘁𝗲𝘀 𝗔 𝗠𝗶 𝗖𝗿𝗲𝗮𝗱𝗼𝗿, si es algo urgente contacta con el a su chat privado para mas informacion*` ,m, { contextInfo:{ externalAdReply: {title: '𝕾𝖆𝖐𝖚𝖗𝖆-𝕭𝖔𝖙-𝕷𝖎𝖙𝖊-𝕸𝕯', body: '𝖭𝗈 𝖤𝗍𝗂𝗊𝗎𝖾𝗍𝖾𝗌 𝖠 𝖬𝗂 𝖢𝗋𝖾𝖺𝖽𝗈𝗋', sourceUrl: 'https://github.com/diegojadibot/SakuraBotLite-MD'}}})
 }
-handler.customPrefix = /@573012482597/i;
+handler.customPrefix = /ES UN POCIO MOLESTO/i;
 handler.command = new RegExp();
 
 export default handler;
