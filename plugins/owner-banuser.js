7d7c8c8beb7460305fe3fde20cf0b28ef7b2d141
REYES <153043081+MESIASREYES@users.noreply.github.com>
2024-04-06 13:33:21 -0600

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -13,7 +13,7 @@ number = text
 }
 user = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
 bot = conn.user.jid.split`@`[0] 
-bant = `🧸 *Etiquete A Una Persona*\n\nEjemplo: !${command} @573218138672`
+bant = `🧸 *Etiquete A Una Persona*\n\nEjemplo: !${command} @50376436909`
 if (!text && !m.quoted) return conn.reply(m.chat, bant, m, { mentions: [user] })               
 try {
 if(text) {
