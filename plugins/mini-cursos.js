21430f9640e8512b3aeb2f3e975f0971cd973014
REYES <153043081+MESIASREYES@users.noreply.github.com>
2024-04-06 18:21:46 -0600

    Update mini-cursos.js

--- plugins/mini-cursos.js
+++ plugins/mini-cursos.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
 
-          m.reply(`_*[ MINI CURSO ]*_\n\n*Entra al numero: +54 9 2266 46-6080 hay encontrarás idiomas basicos de programación!*\n\n*Tambien puedes contactar a: +52 729 488 8993*\n\n_Difruta del mini curso_ 📍`);
+          m.reply(`_*[ MINI CURSO ]*_\n\n*Entra al numero: +503 7643-6909 hay encontrarás idiomas basicos de programación!*\n\n*Tambien puedes contactar a: +44 7863572502*\n\n_Difruta del mini curso_ 📍`);
             });
 }
 handler.help = ['info']
